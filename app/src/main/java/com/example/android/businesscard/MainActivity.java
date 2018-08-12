6cd947438c6006812ebc6c69346db1d352dc2efc
GauravSha <shagaurav@outlook.com>
2018-08-12 13:07:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/businesscard/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.android.businesscard;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    ConstraintLayout myLayout;
+    AnimationDrawable animationDrawable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myLayout = (ConstraintLayout) findViewById(R.id.myLayout);
+
+        animationDrawable = (AnimationDrawable) myLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2500);
+        animationDrawable.setExitFadeDuration(2500);
+        animationDrawable.start();
+    }
+}
